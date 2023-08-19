429067890e2e23da90117a3dfbf13472a5ef3bcc
Albeiro Marin <xlandru@hotmail.com>
2023-08-18 19:41:29 -0500

    cambia variable booleana

--- src/products/services/roles.service.ts
+++ src/products/services/roles.service.ts
@@ -6,6 +6,6 @@ export class RolesService {
     matchRoles(requiredRoles: string[], userRoles: string []): boolean {
         // la lógica de comparación de roles va aquí
 
-        return false // si se retorna false el guard no permite consumir el endpoint y arroja la excepción que hallamos lanzado desde el guard
+        return true // si se retorna false el guard no permite consumir el endpoint y arroja la excepción que hallamos lanzado desde el guard
     }
 }
